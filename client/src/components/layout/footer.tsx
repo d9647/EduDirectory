4e401d85eb9f66ce06773552daa9ed9b07b4c70d
d9647 <wdng9647@gmail.com>
2025-07-27 05:36:44 +0000

    fix footer

--- client/src/components/layout/footer.tsx
+++ client/src/components/layout/footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
         </div>
         <div className="mt-4 md:mt-0 md:order-1">
           <p className="text-center text-xs text-gray-400">
-            &copy; 2025 Education Yellow Pages | Dragonfly AI. All rights reserved.
+            &copy; 2025 Education Yellow Pages | All rights reserved.
           </p>
         </div>
       </div>
